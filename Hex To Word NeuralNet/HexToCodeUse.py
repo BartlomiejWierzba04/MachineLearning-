6b7d7bc8af5b961916d8bc17e4b15b7185eed0ef
Bartlomiej <bartlomiejwierzba04@gmail.com>
2025-02-14 20:37:49 +0000

    changes made to HexToCodeUse.py

--- Hex To Word NeuralNet/HexToCodeUse.py
+++ Hex To Word NeuralNet/HexToCodeUse.py
@@ -1,14 +1,21 @@
 import HexToWord
-import numpy as np
-import webcolors
+#import numpy as np
+#import webcolors
 import tensorflow as tf
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
+import keyboard
+#from sklearn.model_selection import train_test_split
+#from sklearn.preprocessing import LabelEncoder
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Dense, Dropout
 
 
 
 model = tf.keras.models.load_model('model_v1.h5')
 
-print(HexToWord.predict_color("#FF00FF",model))
+
+
+print("Press the UP arrow key to exit...")
+
+while True:
+    hexColor = input("Enter the Hex Colour to get the name") #this can be made into a sanitised input box later - for testing purposes this is fine.
+    print(HexToWord.predict_color(hexColor,model))
